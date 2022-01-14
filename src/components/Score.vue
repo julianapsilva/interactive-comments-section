e589474a65ecf4d7c8d05ded35c53423174ee5c5
julianapsilva <julianadepaula001@gmail.com>
2022-01-14 17:20:42 -0300

    adiciona score

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,20 +1,45 @@
 <template>
-<h1>{{score}}</h1>
+<div class="score-wrapper">
+  <img src="@/assets/icon-plus.svg" alt="icon-plus" srcset="">
+  <span>{{score}}</span>
+  <img src="@/assets/icon-minus.svg" alt="icon-minus">
+</div>
 </template>
 
 <script>
 export default {
   name: 'Score',
   props: {
-      score: Number
+    score: Number,
   },
-  data () {
-    return {}
+  data() {
+    return {};
   },
   components: {},
   methods: {},
-  created () {}
-}
+  created() {},
+};
 </script>
 
-<style></style>
+<style scoped>
+.score-wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 30px;
+  align-items: center;
+  position: absolute;
+  margin: 10px;
+  background-color: hsl(239, 57%, 95%);
+  border-radius: 8px;
+}
+img {
+  width: 12px;
+  padding: 15px;
+}
+span {
+  color: hsl(238, 40%, 52%);
+  font-weight: bold;
+  font-size: 16px;
+}
+
+</style>
